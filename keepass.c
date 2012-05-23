3b0dbfc0ddba9e3fd34c29cf4f19d3851f9321f4
Rob Hoelz <rob@hoelz.ro>
2012-05-23 21:01:30 +0200

    Flesh out exceptions

--- keepass.c
+++ keepass.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <ruby.h>
 #include <kpass.h>
 
@@ -6,8 +7,51 @@
 #define GROUP_CLASS_NAME "Group"
 #define ENTRY_CLASS_NAME "Entry"
 
+static VALUE eException_unknown;
+static VALUE eException_kpass_decrypt_data_fail;
+static VALUE eException_kpass_decrypt_db_fail;
+static VALUE eException_kpass_hash_pw_fail;
+static VALUE eException_kpass_prepare_key_fail;
+static VALUE eException_kpass_load_decrypted_data_entry_fail;
+static VALUE eException_kpass_load_decrypted_data_group_fail;
+static VALUE eException_kpass_init_db_fail;
+static VALUE eException_kpass_encrypt_db_fail;
+static VALUE eException_kpass_encrypt_data_fail;
+static VALUE eException_kpass_pack_db_fail;
+static VALUE eException_kpass_verification_fail;
+static VALUE eException_kpass_unsupported_flag;
+static VALUE eException_kpass_not_implemented;
+
 static void raise_kp_exception(kpass_retval result)
 {
+#define throw_exception(type, msg)\
+    case type:\
+        rb_raise(eException_##type, msg);\
+        break;
+
+    /* it's ok, so don't do anything */
+    if(result == kpass_success) {
+        return;
+    }
+    switch(result) {
+        throw_exception(kpass_decrypt_data_fail, "data decryption failure");
+	throw_exception(kpass_decrypt_db_fail, "database decryption failure");
+	throw_exception(kpass_hash_pw_fail, "password hash failure");
+	throw_exception(kpass_prepare_key_fail, "key preparation failure");
+	throw_exception(kpass_load_decrypted_data_entry_fail, "entry load failure");
+	throw_exception(kpass_load_decrypted_data_group_fail, "group load failure");
+	throw_exception(kpass_init_db_fail, "db init failure");
+	throw_exception(kpass_encrypt_db_fail, "encrypt db failure");
+	throw_exception(kpass_encrypt_data_fail, "encrypt data failure");
+	throw_exception(kpass_pack_db_fail, "pack db failure");
+	throw_exception(kpass_verification_fail, "verification failure");
+	throw_exception(kpass_unsupported_flag, "unsupported flag");
+	throw_exception(kpass_not_implemented, "unimplemented");
+
+        default:
+            rb_raise(eException_unknown, "An unknown error occurred");
+    }
+#undef throw_exception
 }
 
 VALUE
@@ -48,6 +92,54 @@ rb_kp_db_open(VALUE self, VALUE rb_file)
     return Qnil;
 }
 
+static void
+__define_exception(VALUE module, kpass_retval value, const char *value_as_str)
+{
+    char *copy;
+    char *copy_end;
+    char *p;
+
+    copy = strdup(value_as_str + strlen("kpass_")); /* copy and remove
+                                                     * prefix */
+    copy_end = copy + strlen(copy);
+    copy[0]  = toupper(copy[0]); /* upper-case first character */
+
+    p = strchr(copy, '_');
+    while(p) {
+        memmove(p, p + 1, copy_end - p); /* delete '_' character */
+        *p = toupper(*p); /* upper-case character following '_' */
+        p  = strchr(copy, '_');
+    }
+    rb_define_class_under(module, copy, rb_eException);
+    free(copy);
+}
+
+static void
+define_exception_classes(VALUE module)
+{
+#define define_exception(value)\
+    __define_exception(module, value, #value)
+
+    eException_unknown = rb_define_class_under(module, "UnknownException",
+        rb_eException);
+
+    define_exception(kpass_decrypt_data_fail);
+    define_exception(kpass_decrypt_db_fail);
+    define_exception(kpass_hash_pw_fail);
+    define_exception(kpass_prepare_key_fail);
+    define_exception(kpass_load_decrypted_data_entry_fail);
+    define_exception(kpass_load_decrypted_data_group_fail);
+    define_exception(kpass_init_db_fail);
+    define_exception(kpass_encrypt_db_fail);
+    define_exception(kpass_encrypt_data_fail);
+    define_exception(kpass_pack_db_fail);
+    define_exception(kpass_verification_fail);
+    define_exception(kpass_unsupported_flag);
+    define_exception(kpass_not_implemented);
+
+#undef define_exception
+}
+
 void
 Init_keepass(void)
 {
@@ -62,6 +154,8 @@ Init_keepass(void)
         rb_cObject);
     cGroup = rb_define_class_under(mKeepass, GROUP_CLASS_NAME, rb_cObject);
     cEntry = rb_define_class_under(mKeepass, ENTRY_CLASS_NAME, rb_cObject);
+
+    define_exception_classes(mKeepass);
 
     /* Database Methods */
     rb_define_method(cDatabase, "open", rb_kp_db_open, 1);
