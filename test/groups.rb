464d492883f241497a782347ff56c2d74bf8c648
Rob Hoelz <rob@hoelz.ro>
2012-05-26 15:26:34 +0200

    Test time methods for groups

--- test/groups.rb
+++ test/groups.rb
@@ -3,6 +3,7 @@ require 'keepass'
 
 KP_FILE            = 'test/example.kdb'
 CORRECT_PASSWORD   = 'abc123'
+TIME_METHODS       = [ :ctime, :mtime, :atime, :etime ]
 
 class TestKeepass_Groups < MiniTest::Unit::TestCase
     def test_group_names
@@ -14,5 +15,16 @@ class TestKeepass_Groups < MiniTest::Unit::TestCase
         end
 
         assert_equal seen_groups, %w(Test1 Test2)
+    end
+
+    def test_time_methods
+        kdb   = Keepass::Database.open KP_FILE, CORRECT_PASSWORD
+        group = kdb.groups[0]
+
+        times = TIME_METHODS.collect do |method|
+            (group.send method).class
+        end
+
+        assert_equal times, [ Time ] * TIME_METHODS.length
     end
 end
